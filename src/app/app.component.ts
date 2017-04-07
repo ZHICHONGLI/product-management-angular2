d6f8c82856525d092da1e4c7957ab5ab7137df7d
Luke Li <leezhc@gmail.com>
2017-04-06 20:18:45 -0700

    add go to top button, fix cardheader size

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Inject } from '@angular/core';
+import { DOCUMENT } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+ // public showBtn: boolean = true;
+
+  constructor(@Inject(DOCUMENT)private document: Document){
+
+  }
+  @HostListener('window:scroll',[])
+  onWindowScroll(){
+    let offsetValue = this.document.body.scrollTop;
+    if(offsetValue > 200){
+      this.document.getElementById('topbtn').style.display = 'block';
+      
+    }else if(offsetValue < 200){
+      this.document.getElementById('topbtn').style.display = 'none';
+    }
+  }
+
+  goTop(){
+    window.scrollTo(20, 0);
+  }
 }
